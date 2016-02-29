05bd7d5910c69df61ba0f879196e0eb77964cc2f
Antonio Murdaca <runcom@redhat.com>
2016-02-29 11:49:35 +0100

    initial config
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=${NAME:-registry}
+
+docker run -d -p 5000:5000 --restart=always --name $name \
+	-v `pwd`/certs:/certs \
+	-e REGISTRY_HTTP_TLS_CERTIFICATE=certs/domain.crt \
+	-e REGISTRY_HTTP_TLS_KEY=certs/domain.key \
+	registry:2.3.0
---
+++ launch_v1.sh
@@ -0,0 +1,1 @@
+docker run -d --restart=always -p 5002:5000 --name registryv1 registry:0.9.1
